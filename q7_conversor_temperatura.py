49c2704172a325fef52bc17eeadbb4f45b03a5ff
Francisco-Cassio <franciscodecassiomourao@gmail.com>
2025-04-25 15:13:38 -0300

    exercício de condicionais

--- q7_conversor_temperatura.py
+++ q7_conversor_temperatura.py
@@ -4,28 +4,8 @@ from funcoes import linhas
 def main():
     temperatura = obter_numero_real('Quantos graus deseja? ')
     linhas()
-    tipo_temp = input('Qual tipo de temperatura deseja escolher? (C/F/K): ')
-    tipo_convert = input('Para qual tipo deseja converter? (C/F/K): ')
 
-    linhas()
-
-    if eh_celsius(tipo_temp):
-        if temperatura >= -273.15:
-            conversao_celsius(temperatura, tipo_convert)
-        else:
-            print('A temperatura em Celsius não pode ser menor que -273.15 °C')
-
-    elif eh_fahrenheit(tipo_temp):
-        conversao_fahrenheit(temperatura, tipo_convert)
-    
-    elif eh_kelvin(tipo_temp):
-        if temperatura >= 0:
-            conversao_kelvin(temperatura, tipo_convert)
-        else:
-            print('A temperatura em Kelvin deve ser maior que 0')
-            
-    else:
-        print('As letras digitadas devem corresponder as unidades de temperatura (C/F/K)')
+    validacao_conversao(temperatura)
 
 
 def eh_celsius(tipo: str):
@@ -61,4 +41,38 @@ def conversao_kelvin(temperatura: int, convertido: str):
         print(f'{temperatura} k equivale a {((temperatura * 1.8) - 459.67):.1f} °F')
 
 
+def validacao_conversao(temperatura: int):
+    tipo_temp = input('Qual tipo de temperatura deseja escolher? (C/F/K): ')
+    tipo_convert = input('Para qual tipo deseja converter? (C/F/K): ')
+    
+    linhas()
+
+    if not eh_celsius(tipo_temp) or not eh_fahrenheit(tipo_temp) or not eh_kelvin(tipo_temp) or not eh_celsius(tipo_convert) or not eh_fahrenheit(tipo_convert) or not eh_kelvin(tipo_convert):
+        print('As letras digitadas devem corresponder às unidades de temperatura')
+        linhas()
+        return validacao_conversao(temperatura)
+    
+
+    if tipo_temp == tipo_convert:
+        print('As unidades de temperatura devem ser diferentes')
+        return validacao_conversao(temperatura)
+
+
+    if eh_celsius(tipo_temp):
+        if temperatura >= -273.15:
+            conversao_celsius(temperatura, tipo_convert)
+        else:
+            print('A temperatura em Celsius não pode ser menor que -273.15 °C')
+
+    elif eh_fahrenheit(tipo_temp):
+        conversao_fahrenheit(temperatura, tipo_convert)
+    
+    elif eh_kelvin(tipo_temp):
+        if temperatura >= 0:
+            conversao_kelvin(temperatura, tipo_convert)
+        else:
+            print('A temperatura em Kelvin deve ser maior que 0')
+
+
 main()
+    
